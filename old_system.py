3627fcb89711664e8a5ca09d11b664b502af7c15
Sonia-P5 <sp102@kent.ac.uk>
2026-02-06 15:17:17 +0000

    Mistake 1 Attempt 5

--- old_system.py
+++ old_system.py
@@ -1,37 +1,6 @@
-def main(): #Mistake 1: Not including def main() and main()
+def main(): #Mistake 1: Did not properly defined function, did not include def main() and main()
     n = ["Picard", "Riker", "Data", "Worf"]
     r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
     d = ["Command", "Command", "Operations", "Security"]
 
-    active=True
-
-    def run_system_monolith():
-        print("BOOTING SYSTEM...")
-        print("...")
-        print("WELCOME TO FLEET COMMAND")
-    run_system_monolith() #Mistake 2: Not properly definiing function
-
-    def loading(): #Mistake 3, did not define loading
-        loading = 0
-        while loading < 5:
-            print("Loading module " + str(loading))
-            break #Mistake 4, did not add break
-
-        while True:
-            print("\n--- MENU ---")
-            print("1. View Crew")
-            print("2. Add Crew")
-            print("3. Remove Crew")
-            print("4. Analyze Data")
-            print("5. Exit")
-
-            opt = input("Select option: ")
-
-            if opt == "1":  #Mistake 5: Did not put two equal signs
-                print("Current Crew List:")
-                break
-            break #Mistake 6 Break
-
-
-    
 main()
