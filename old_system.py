7d5b7a6d52a067dd35416229b20344f212c991d3
Sonia-P5 <sp102@kent.ac.uk>
2026-02-06 13:23:11 +0000

    Mistake 2: Attempt 3

--- old_system.py
+++ old_system.py
@@ -9,4 +9,5 @@ def main(): #Mistake 1: Forgot to put def main() and main()
         print("BOOTING SYSTEM...")
         print("...")
         print("WELCOME TO FLEET COMMAND")
+    run_system_monolith() #Mistake 2: Did not properly define the function, forgot to add this
 main()
