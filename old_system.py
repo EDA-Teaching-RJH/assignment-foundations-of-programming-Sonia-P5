6837c00418138d4c671ae85dcdaba609af669408
Sonia-P5 <sp102@kent.ac.uk>
2026-02-06 15:18:12 +0000

    Mistake 2 attempt 5

--- old_system.py
+++ old_system.py
@@ -3,4 +3,17 @@ def main(): #Mistake 1: Did not properly defined function, did not include def m
     r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
     d = ["Command", "Command", "Operations", "Security"]
 
+    active = True
+
+    def run_system_monolith():
+        print("BOOTING SYSTEM...")
+        print("...")
+        print("WELCOME TO FLEET COMMAND")
+    run_system_monolith() #Mistake 2: Did not properly define function, forgot to add this
+    
+    
+        loading = 0
+        while loading < 5:
+            print("Loading module " + str(loading))
+
 main()
