23dbaea24db60f3c06474dd56040b3113014d400
Sonia-P5 <sp102@kent.ac.uk>
2026-02-06 15:27:35 +0000

    Mistake 1: Attempt 6

--- old_system.py
+++ old_system.py
@@ -1,4 +1,4 @@
-def main(): #Mistake 1: Did not properly defined function, did not include def main() and main()
+def main(): #Mistake 1: Did not include def main() and main()
     n = ["Picard", "Riker", "Data", "Worf"]
     r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
     d = ["Command", "Command", "Operations", "Security"]
@@ -9,91 +9,5 @@ def main(): #Mistake 1: Did not properly defined function, did not include def m
         print("BOOTING SYSTEM...")
         print("...")
         print("WELCOME TO FLEET COMMAND")
-    run_system_monolith() #Mistake 2: Did not properly define function, forgot to add this
-    
-    def loading(): #Mistake 3: Forgot to define function
-        loading = 0
-        while loading < 5:
-            print("Loading module " + str(loading))
-            break #Mistake 4: Forgot to add 
-        
-        while True:
-            print("\n--- MENU ---")
-            print("1. View Crew")
-            print("2. Add Crew")
-            print("3. Remove Crew")
-            print("4. Analyze Data")
-            print("5. Exit")
-
-            opt = input("Select option: ")
-
-            if opt =="1":  #Mistake 5: Did not put double equals sign
-                print("Current Crew List:")
-            
-                for i in range(10):
-                    print(n[i] + " - " + r[i])
-                    break #Mistake 6: Did not add break
-                break #Mistake 6: Did not add break
-
-            elif opt == "2":
-                new_name = input("Name: ")
-                new_rank = input("Rank: ")
-                new_div = input("Division: ")
-            
-           
-                n.append(new_name)
-                print("Crew member added.")
-                break #Mistake 7: Did not add break
-
-            elif opt == "3":
-                rem = input("Name to remove: ")
-           
-                idx = n.index(rem)
-                n.pop(idx)
-                r.pop(idx)
-                d.pop(idx)
-                print("Removed.")
-                break #Mistake 8: Add Break
-
-            elif opt == "4":
-                print("Analyzing...")
-                count = 0
-            
-                for rank in r:
-                    if rank == "Captain" or "Commander": 
-                        count = count + 1
-                    print("High ranking officers: " + str(count)) #Mistake 9: Type cast as string
-                break #Mistake 10: Break
-
-            elif opt == "5":
-                print("Shutting down.")
-                break
-            
-            else:
-                print("Invalid.")
-
-        x = 10 #Mistake 11: Indendetation Issue
-        if x > 5:
-            print("System Check OK")
-        else:
-            print("System Failure")
-            
-       
-        if len(n) > 0:
-            print("Database has entries.")
-        if len(n) == 0:
-            print("Database empty.")
-
-        
-        fuel = 100
-        consumption = 0
-        while fuel > 0:
-            
-            print("Idling...")
-            break 
-            
-        print("End of cycle.")
-
-
 
 main()
