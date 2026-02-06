af34943cdb20b6a4ec5ff180a85b40b5caafd509
Sonia-P5 <sp102@kent.ac.uk>
2026-02-06 15:51:10 +0000

    Mistake 2 Attempt 8

--- old_system.py
+++ old_system.py
@@ -2,4 +2,12 @@ def main(): #Mistake 1: Did not add def main() and main()
     n = ["Picard", "Riker", "Data", "Worf"]
     r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
     d = ["Command", "Command", "Operations", "Security"]
+
+    active = True
+
+    def run_system_monolith():
+        print("BOOTING SYSTEM...")
+        print("...")
+        print("WELCOME TO FLEET COMMAND")
+    run_system_monolith() #Mistake 2: Did not properly defin function, forgot to add this
 main()
