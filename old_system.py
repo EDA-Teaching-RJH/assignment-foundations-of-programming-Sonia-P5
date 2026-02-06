6a1aeaf6c321f97a0ac717206af37a35318c1157
Sonia-P5 <sp102@kent.ac.uk>
2026-02-06 13:25:00 +0000

    Mitake 3 Attempt 3

--- old_system.py
+++ old_system.py
@@ -10,4 +10,9 @@ def main(): #Mistake 1: Forgot to put def main() and main()
         print("...")
         print("WELCOME TO FLEET COMMAND")
     run_system_monolith() #Mistake 2: Did not properly define the function, forgot to add this
+
+    def loading(): #Mistake 3: Did not Define Loading
+        loading = 0
+        while loading < 5:
+            print("Loading module " + str(loading))
 main()
