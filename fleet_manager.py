7dbf54b9f04c960a66e4f80d833bd77fe3a98b9f
Sonia-P5 <sp102@kent.ac.uk>
2026-02-07 08:45:11 +0000

    adding lists

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,3 +1,5 @@
 def main():
-
+    n = ["Picard", "Riker", "Data", "Worf"]
+    r = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+    d = ["Command", "Command", "Operations", "Security"]
 main()
